c8579d075897eabae7b0c7f47dfedbe1bbcd4ba8
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-08-20 23:09:49 +0200

    updated user list

--- src/app/components/user-list/user-list-component.ts
+++ src/app/components/user-list/user-list-component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../services/user-service';
 import { User } from '../../interfaces/user';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-list-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-list-component.html',
   styleUrl: './user-list-component.css'
 })
 
 export class UserListComponent {
 
-  private users: User[] = [];
+  public users: User[] = [];
 
   constructor(private userService: UserService) { }
 
