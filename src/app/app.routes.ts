62bfb5ea12a5224225b3e3ca2448a1849bdcd5a9
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-08-21 09:17:14 +0200

    updated user list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
-import { Routes } from '@angular/router';
+import { provideRouter, Routes, withDebugTracing } from '@angular/router';
 import { UserListComponent } from './components/user-list/user-list-component';
+import { ApplicationConfig } from '@angular/core';
 
 export const routes: Routes = [
     { path: '', component: UserListComponent },
 ];
+
+export const appConfig: ApplicationConfig = {
+    providers: [provideRouter(routes, withDebugTracing())]
+}
